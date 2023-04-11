5a2683d3d2975f850c8e5381eb7ef29afa605698
Eduard Camilo Ortega Sanchez <codecos.info@gmail.com>
2023-04-11 16:57:54 -0500

    add exchangerate api

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ const worthSecondEl = document.getElementById('worth-second');
 
 const exchangeRateEl = document.getElementById('exchange-rate');
 
+updateRate()
+
 function updateRate(){
-    console.log('Called');
+    fetch(`https://v6.exchangerate-api.com/v6/547c421040ebd6518c1058e8/latest/${currencyFirstEl.value}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        const rate = data.conversion_rates[currencySecondEl.value];
+        console.log(rate);
+        exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`;
+
+        worthSecondEl.value = (worthFirstEl.value * rate).toFixed(4)
+    });
 }
 
 currencyFirstEl.addEventListener('change', updateRate)
